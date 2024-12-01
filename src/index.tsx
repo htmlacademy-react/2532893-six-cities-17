abd551eb2f513e3f6aa9780f3855a191489ec9bb
Yuriy Andreev <andreyev.yury@gmail.com>
2024-12-01 19:24:24 +0300

    Typescript error(

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/app.tsx';
 import {offersData} from './mocks/offers.ts';
+import {commentsData} from './mocks/review.tsx';
 
 
 const root = ReactDOM.createRoot(
