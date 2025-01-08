24dadbd36c64baff0beef510acb503f0e9dfb8a6
Yuriy Andreev <andreyev.yury@gmail.com>
2025-01-08 18:10:33 +0300

    remove mocks

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,30 +1,30 @@
 import {CITIES_LIST, defaultCityType} from '../mocks/cities-list.ts';
-import {IMocksData, OFFERS_DATA_MOCK} from '../mocks/offers.ts';
+import {IMocksData} from '../mocks/offers.ts';
 import {createReducer} from '@reduxjs/toolkit';
-import {changeActiveCity, showCityOffers, loadOffers} from './action.ts';
+import {changeActiveCity, loadOffers} from './action.ts';
 import {ReducerWithInitialState} from '@reduxjs/toolkit/dist/createReducer';
 
 const defaultCity: defaultCityType = CITIES_LIST[0];
 
 export type initialStateType = {
   activeCityName: string;
-  cityOffers: IMocksData[];
+  // cityOffers: IMocksData[];
   offers: IMocksData[];
 }
 
 export const initialState: initialStateType = {
   activeCityName: defaultCity.title,
-  cityOffers: OFFERS_DATA_MOCK.filter((item) => item.city.name === defaultCity.title),
-  offers,
+  // cityOffers: [].filter((item) => item.city.name === defaultCity.title),
+  offers: [],
 };
 
 export const reducer: ReducerWithInitialState<initialStateType> = createReducer(initialState, (builder) => {
   builder.addCase(changeActiveCity, (state, action) => {
     state.activeCityName = action.payload;
   });
-  builder.addCase(showCityOffers, (state, action) => {
-    state.cityOffers = action.payload;
-  });
+  // builder.addCase(showCityOffers, (state, action) => {
+  //   state.cityOffers = action.payload;
+  // });
   builder.addCase(loadOffers, (state, action) => {
     state.offers = action.payload;
   });
