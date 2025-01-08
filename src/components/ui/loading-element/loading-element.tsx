8a61068f6bd26612ac93db33b498aec6c42db958
Yuriy Andreev <andreyev.yury@gmail.com>
2025-01-08 20:53:16 +0300

    add new loader

--- src/components/ui/loading-element/loading-element.tsx
+++ src/components/ui/loading-element/loading-element.tsx
@@ -1,8 +1,21 @@
-
+import './loading-element.css';
 
 export function LoadingElement(): JSX.Element{
   return (
-    <p>...Loading</p>
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   );
 }
 
