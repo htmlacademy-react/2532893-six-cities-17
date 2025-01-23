b79a9017e3c1163b9a0f80d41292be4782974422
Yuriy Andreev <andreyev.yury@gmail.com>
2025-01-23 20:55:41 +0300

    user-process

---
+++ src/store/user-process/user-process.ts
@@ -0,0 +1,38 @@
+
+import {LoginStatus} from '../../data/login-status.ts';
+import {createSlice} from '@reduxjs/toolkit';
+import {Namespace} from '../namespace.ts';
+import {fetchAuthorizationStatus, loginAction, logoutAction} from '../api-actions.ts';
+
+export type UserProcessType = {
+  authorizationStatus: LoginStatus;
+};
+
+const initialState = {
+  authorizationStatus: LoginStatus.Unknown,
+};
+
+export const userProcess = createSlice({
+  name: Namespace.User,
+  initialState,
+  reducers: {},
+  extraReducers(builder){
+    builder
+      .addCase(fetchAuthorizationStatus.fulfilled, (state) => {
+        state.authorizationStatus = LoginStatus.Auth;
+      })
+      .addCase(fetchAuthorizationStatus.rejected, (state) => {
+        state.authorizationStatus = LoginStatus.NoAuth;
+      })
+      .addCase(loginAction.fulfilled, (state) => {
+        state.authorizationStatus = LoginStatus.Auth;
+      })
+      .addCase(logoutAction.fulfilled, (state) => {
+        state.authorizationStatus = LoginStatus.NoAuth;
+      })
+      .addCase(logoutAction.rejected, (state) => {
+        state.authorizationStatus = LoginStatus.Auth;
+      })
+    ;
+  }
+});
