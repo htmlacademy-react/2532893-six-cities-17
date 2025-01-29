d66caef599515cabcac33c9f64d31873caf2d7cd
Yuriy Andreev <andreyev.yury@gmail.com>
2025-01-29 18:30:57 +0300

    работает отправка комментария и обнулений полей ввода

--- src/components/ui/comment-send-star-input/comment-send-star-input.tsx
+++ src/components/ui/comment-send-star-input/comment-send-star-input.tsx
@@ -5,13 +5,14 @@ import React from 'react';
 type RatingStarsType = {
   onChange: React.ChangeEventHandler<HTMLElement>;
   value: number;
+  isChecked: boolean;
 }
 
-export function CommentSendStarInput({onChange, value}: RatingStarsType): JSX.Element{
+export function CommentSendStarInput({onChange, value, isChecked}: RatingStarsType): JSX.Element{
 
   return(
     <>
-      <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio" onChange={onChange}/>
+      <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio" onChange={onChange} checked={isChecked}/>
       <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title="good">
         <svg className="form__star-image" width="37" height="33">
           <use xlinkHref="#icon-star"></use>
--- src/store/data-process/data-selectors.ts
+++ src/store/data-process/data-selectors.ts
@@ -6,3 +6,4 @@ export const getOffers = (state: StateType) => state[Namespace.Data].offers;
 export const getOffersDataLoading = (state: StateType) => state[Namespace.Data].isOffersDataLoading;
 export const getNearbyOffersList = (state: StateType) => state[Namespace.Data].nearbyOffers;
 export const getCommentsList = (state: StateType) => state[Namespace.Data].comments;
+export const getReviewPostingStatus = (state: StateType) => state[Namespace.Data].reviewPostingStatus;
